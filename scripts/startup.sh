15e3ee6fdd36ebb26743ba988df12fcb16954ad3
Mike Perry <mikeperry-git@torproject.org>
2018-05-26 22:17:48 +0000

    Fix twistd port arg.

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -6,8 +6,7 @@ openssl req \
         -out /etc/nginx/ssl/nginx.crt;
 cp /config/nginx.conf /etc/nginx/sites-available/default;
 /etc/init.d/nginx reload;
-cd onionperf;
 # onionperf measure --tor=/tor/src/or/tor \
 # --tgen=/shadow/src/plugin/shadow-plugin-tgen/build/tgen --twistd=/usr/local/bin/twistd;
 twistd --version
-/usr/local/bin/twistd -n -l - web  --path /onionperf/onionperf-data/twistd/docroot --port 8090 --mime-type=None
+/usr/local/bin/twistd -n -l - web  --path /onionperf/onionperf-data/twistd/docroot --port tcp:8090 --mime-type=None
